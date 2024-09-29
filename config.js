58fb2aa6ea00613cef95ea7a08afe3eebccb0ce4
Kappi7581 <Kappi7581@outlook.com>
2024-09-29 08:02:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-  TOKEN: "", //write your discord bot token
-  authorID: "", //write your discord account id.
-  url: "", //Type the Custom Discord Server URL You Want to Retrieve!
-  serverID: "", //Type the Server ID to Get the Url!
-  log: "", //If the URL is taken, write the Log Channel ID to be informed!
+  TOKEN: "MTI3MzI0NDE0ODI3NDY5MjE0OA.Gn5YHC.5No13j5aYDeUWTPvVB3Ora0Wdy0LBe1yD0T6Lc", //write your discord bot token
+  authorID: "1272964504657068197", //write your discord account id.
+  url: "kappi", //Type the Custom Discord Server URL You Want to Retrieve!
+  serverID: "1276270914849210428", //Type the Server ID to Get the Url!
+  log: "1276274674501685342", //If the URL is taken, write the Log Channel ID to be informed!
   status: "true", //false=Stop Bot | true=Means Start the Bot!
   botRun: "10" //1000 = 1 Second | 1 = 1 Millisecond | It is recommended to write at least 50 milliseconds. Under 30MS, the project will be banned from the Discord API and will stop working!
 
